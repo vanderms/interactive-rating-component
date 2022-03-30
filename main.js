6ed73abd9d36886de1514d55ece655434594c6c2
vanderms <vanderms.84@gmail.com>
2022-03-30 09:57:47 -0300

    first comit

---
+++ main.js
@@ -0,0 +1,15 @@
+({
+  ratingComponent: document.querySelector('.nv-rating-component'),
+  form: document.querySelector('.form'),
+  modal: document.querySelector('.nv-modal'),
+  init() {
+    this.form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const rating = this.form.rating.value;
+      const userRatingParagraph = this.modal.querySelector('.user-rating');
+      userRatingParagraph.textContent = `You selected ${rating} out of 5`;
+      this.ratingComponent.dataset.state = 'hidden';
+      this.modal.dataset.state = 'display';
+    });
+  },
+}.init());
